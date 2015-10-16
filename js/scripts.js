e11e2dad90f285dae6d26749a4afef1b7742ade0
supaheckafresh <neilstrain@gmail.com>
2015-10-15 23:37:33 -0700

    minor refactoring of display code

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 
-var itemText_input = document.getElementById('enterItem');
+var addItem_input = document.getElementById('enterItem');
 var toDoList_ul = document.getElementById('toDoList');
 
 var toDoItems = [];
@@ -9,19 +9,20 @@ var addItem_button = document.getElementById('itemSubmit');
 //TODO remove debug console logs
 
 //TODO add edit for selected tasks
+//TODO make item not editable when marked complete
 
 //TODO MAYBE - add theme selector?
 
 addItem_button.addEventListener('click', function (evt) {
-    if (itemText_input.value) {
-        toDoItems.push(itemText_input.value.trim());
+    if (addItem_input.value) {
+        toDoItems.push(addItem_input.value.trim());
 
         evt.preventDefault();
 
         displayToDoItems();
-        alert(itemText_input.value + ' has been added to your list!');
+        alert(addItem_input.value + ' has been added to your list!');
 
-        itemText_input.value = '';
+        addItem_input.value = '';
     } else {
         evt.preventDefault();
     }
@@ -41,13 +42,13 @@ function displayItem(item) {
     span.setAttribute('class', 'item-text col-xs-10');
     span.appendChild(document.createTextNode(item));
 
-    var div = document.createElement('div');
-    div.setAttribute('class', 'to-do-item');
-
     var li = document.createElement('li');
     li.appendChild(displayItem.checkbox());
     li.appendChild(span);
     li.setAttribute('class', 'list-group-item list-group-item-danger col-xs-7 col-xs-offset-1');
+
+    var div = document.createElement('div');
+    div.setAttribute('class', 'to-do-item');
 
     div.appendChild(li);
     div.appendChild(displayItem.button('glyphicon glyphicon-edit'));
@@ -64,13 +65,12 @@ displayItem.checkbox = function() {
 };
 
 displayItem.button = function(buttonType) {
-    var btn = document.createElement('button');
-
     var span = document.createElement('span');
     span.setAttribute('class', buttonType);
     span.setAttribute('aria-hidden', 'true');
-    btn.appendChild(span);
 
+    var btn = document.createElement('button');
+    btn.appendChild(span);
     btn.setAttribute('type', 'button');
     btn.setAttribute('class', 'btn btn-default btn-lg col-xs-1');
     return btn;
